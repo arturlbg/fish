b287e5a52c8ff33fb3895b0cf32c4a582a31db5a
Artur Luis <60628919+arturlbg@users.noreply.github.com>
2021-10-12 18:53:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -32,10 +32,20 @@ function wait(time) {
 async function routine(claim = true, fishIndex) {
   console.log("[INFO] Running routine");
   if (claim) {
-    await axios.post(BASE_URL, { data: CLAIM_TOKEN[fishIndex], name: "claim_reward" });
+    //await axios.post(BASE_URL, { data: CLAIM_TOKEN[fishIndex], name: "claim_reward" });
+    await axios.post("https://apius.reqbin.com/api/v1/requests",  {"id":"0","name":"","errors":"","json":`{\"method\":\"POST\",\"url\":\"https://api.fishytank.io/rpc\",\"apiNode\":\"US\",\"contentType\":\"JSON\",\"content\":\"{\\\"name\\\":\\\"claim_reward\\\",\\\"data\\\":\\\"${CLAIM_TOKEN[fishIndex]}\\\"}\",\"headers\":\"\",\"errors\":\"\",\"curlCmd\":\"\",\"codeCmd\":\"\",\"auth\":{\"auth\":\"noAuth\",\"bearerToken\":\"\",\"basicUsername\":\"\",\"basicPassword\":\"\",\"customHeader\":\"\",\"encrypted\":\"\"},\"compare\":false,\"idnUrl\":\"https://api.fishytank.io/rpc\"}`,"deviceId":"b95a04cc-7065-49f9-9e9f-6a63e11e690eR"}, {
+      headers:{
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36 OPR/79.0.4143.73",
+      'Accept-Language': 'en'
+    }});
     await wait(5000);
   }
-  await axios.post(BASE_URL, { data: FEED_TOKEN[fishIndex], name: "feed_for_fish" });
+  //await axios.post(BASE_URL, { data: FEED_TOKEN[fishIndex], name: "feed_for_fish" });
+  await axios.post("https://apius.reqbin.com/api/v1/requests",  {"id":"0","name":"","errors":"","json":`{\"method\":\"POST\",\"url\":\"https://api.fishytank.io/rpc\",\"apiNode\":\"US\",\"contentType\":\"JSON\",\"content\":\"{\\\"name\\\":\\\"feed_for_fish\\\",\\\"data\\\":\\\"${FEED_TOKEN[fishIndex]}\\\"}\",\"headers\":\"\",\"errors\":\"\",\"curlCmd\":\"\",\"codeCmd\":\"\",\"auth\":{\"auth\":\"noAuth\",\"bearerToken\":\"\",\"basicUsername\":\"\",\"basicPassword\":\"\",\"customHeader\":\"\",\"encrypted\":\"\"},\"compare\":false,\"idnUrl\":\"https://api.fishytank.io/rpc\"}`,"deviceId":"b95a04cc-7065-49f9-9e9f-6a63e11e690eR"}, {
+      headers:{
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36 OPR/79.0.4143.73",
+      'Accept-Language': 'en'
+    }});
   fishes[fishIndex].running = false
   await startup();
 }
@@ -48,12 +58,25 @@ function prepareRun(fishIndex, when){
 }
 
 async function startup() {
-  const { data } = await axios.post(BASE_URL, {
-    data: GET_FISH_TOKEN,
-    name: "get_fish_in_tank",
-  });
+    // const { data } = await axios.post(BASE_URL, {
+    //   data: GET_FISH_TOKEN,
+    //   name: "get_fish_in_tank",
+    // }, {
+    //   headers:{
+    //   origin: "https://app.fishytank.io",
+    //   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36 OPR/79.0.4143.73",
+    //   'Accept-Language': 'en'
+    // }});
 
-  data.data.forEach(async (fish, index, arr) => {
+const {data} = await axios.post("https://apius.reqbin.com/api/v1/requests",  {"id":"0","name":"","errors":"","json":"{\"method\":\"POST\",\"url\":\"https://api.fishytank.io/rpc\",\"apiNode\":\"US\",\"contentType\":\"JSON\",\"content\":\"{\\\"name\\\":\\\"get_fish_in_tank\\\",\\\"data\\\":\\\"ZWgsFJJiQnJPdZXnKorQZ8/yE0kxc5P1THHGQmR7oQoNCUmLqnchvOC8CDbJvval/h+Ahsr+OSw=\\\"}\",\"headers\":\"\",\"errors\":\"\",\"curlCmd\":\"\",\"codeCmd\":\"\",\"auth\":{\"auth\":\"noAuth\",\"bearerToken\":\"\",\"basicUsername\":\"\",\"basicPassword\":\"\",\"customHeader\":\"\",\"encrypted\":\"\"},\"compare\":false,\"idnUrl\":\"https://api.fishytank.io/rpc\"}","deviceId":"b95a04cc-7065-49f9-9e9f-6a63e11e690eR"}, {
+      headers:{
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36 OPR/79.0.4143.73",
+      'Accept-Language': 'en'
+    }});
+   
+  const jsonResponse = JSON.parse(data.Content)
+
+  jsonResponse.data.forEach(async (fish, index, arr) => {
     if(fishes.length < arr.length){
       fishes.push({name: fish.name, running: false});
     }
